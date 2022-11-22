d3059361c5f301fe7332df108016f0b840f64f02
theEpicMouse90 <68754253+theEpicMouse90@users.noreply.github.com>
2022-11-22 15:20:32 -0600

    Trip changed speed at oil rig

--- Trip2python.py
+++ Trip2python.py
@@ -3,9 +3,9 @@ from spike.control import wait_for_seconds, wait_until, Timer
 from spike.operator import equal_to
 from time import sleep
 from math import *
- 
+
 hub = PrimeHub()
- 
+
 # Sensors
 colorF = ColorSensor('F')
 # Motors
@@ -18,7 +18,7 @@ def PID(distance, speed, targ = 0, kp = 0.8, ki = 0.005, kd = 0.2):
     hub.motion_sensor.reset_yaw_angle()
     LM.set_degrees_counted(0)
     RM.set_degrees_counted(0)
- 
+
     curErr = 0
     prevErr = 0
     accErr = 0
@@ -52,7 +52,7 @@ def PID(distance, speed, targ = 0, kp = 0.8, ki = 0.005, kd = 0.2):
             PID = P + I + D
             motors.start_tank_at_power(floor(speed + PID), floor(speed - PID))
     motors.stop()
- 
+
 def turn(angle, speed):
     hub.motion_sensor.reset_yaw_angle()
     if angle >= 0:
@@ -69,35 +69,36 @@ def turn(angle, speed):
 def LowerArm(speed):
     CA.set_degrees_counted(0)
     CA.run_for_degrees(-370, speed)
- 
- 
+
+
 def Trip7():
-    PID(0.9 * 360, 50)
+    PID(360, 50)
     turn(45, 50)
     PID(1.8 * 360, 50)
     turn(-45, 50)
-    PID(360, 50)
+    PID(0.75 * 360, 100)
     sleep(0.75)
     PID(0.4 * 360, -50)
     turn(45, 50)
-    PID(0.64 * 360, 50)
+    PID(0.82 * 360, 50)
     turn(30, 50)
-    PID(2.5 * 360, -50)
-    PID(360, 50)
-    sleep(0.1)
-    PID(1.5 * 360, -50)
- 
- 
- 
- 
+    PID(2 * 360, -100)
+    PID(0.5 * 360, 100)
+    PID(360, -100)
+    PID(180, 100)
+    PID(360, -100)
+    PID(180, 100)
+    # turn(45, 50)
+    # PID(90, 50)
+    # turn(-135, 50)
+    # PID(180, 50)
+    # sleep(0.1)
+    # PID(180, -50)
+    hub.status_light.on('azure')
+
+
 def returnBack():
     turn(-45, 50)
     PID(3.5 * 360, -50)
-# returnBack()
- 
-# CA.set_degrees_counted(0)
-Trip7()
-# LowerArm(50)
-# PID(1*360, -50)
-# PID(4*360, 50)
 
+Trip7()
